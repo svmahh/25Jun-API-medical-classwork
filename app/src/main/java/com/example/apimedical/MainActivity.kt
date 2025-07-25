7f0ea16559934eae07d2be535dcc22fe9e1f2b6c
Sumah3 <126692045+Sumah3@users.noreply.github.com>
2025-07-25 16:34:32 +0200

    idk, do hw fix it

--- app/src/main/java/com/example/apimedical/MainActivity.kt
+++ app/src/main/java/com/example/apimedical/MainActivity.kt
@@ -9,11 +9,14 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.github.kittinunf.fuel.gson.jsonBody
 import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.fuel.httpPost
 import com.google.gson.Gson
 import com.google.gson.JsonSyntaxException
 import java.util.concurrent.Executors
@@ -39,10 +42,31 @@ class MainActivity : AppCompatActivity() {
 
         outputTextView = findViewById(R.id.txtOutput)
         inputIdEditText = findViewById(R.id.etInputId)
+
         findViewById<Button>(R.id.btnGetAll).setOnClickListener {
             hideKeyboard()
             getAllLoans()
+        }
 
+        findViewById<Button>(R.id.btnGetAllById).setOnClickListener {
+            hideKeyboard()
+            val idText = inputIdEditText.text.toString()
+            if (idText.isNotEmpty()){
+                try{
+                    //try to convert the input to an integer
+                    val id = idText.toInt()
+                    getLoanbyID(id)
+                }
+                catch (e: NumberFormatException){
+                    // handle cases where the input is not a valid number.
+                    Toast.makeText(this, "Invalid input. Please enter a valid number.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        findViewById<Button>(R.id.btnCreate).setOnClickListener {
+            hideKeyboard()
+            createNewLoan()
         }
 
     }
@@ -96,9 +120,114 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun getLoanbyID(id:Int){
+        // Define the API endpoint URL
+        val url = "https://opsc.azurewebsites.net/loans/$id"
+        outputTextView.text = "Fetching loan with ID : $id..."
+
+        //execute the network request on a background thread
+        executor.execute {
+            url.httpGet().responseString { _, response, result ->
+                //switch to the main thread to update the UI
+                handler.post {
+                    if (response.statusCode == 404){
+                        outputTextView.text = "Loan with ID $id not found"
+                        return@post
+                    }
+
+                    when (result) {
+                        is Result.Success ->{
+                            try{
+                            val loan = Gson().fromJson(result.get(), Loan::class.java)
+                            val formattedOutput = "Loan ID: ${loan.LoanID}\nAmount: ${loan.amount}"+
+                                    "${loan.memberID}\nMessage: ${loan.message}"
+                            outputTextView.text = formattedOutput
+                        }
+                        catch (e: JsonSyntaxException) {
+                                // Handle cases where the server response is not valid JSON
+                                Log.e("GetLoanbyID", "JSON parsing error: ${e.message}")
+                                outputTextView.text = "could not parse server response."
+                            }
+                        }
+
+                        is Result.Failure -> {
+                            //on failure, log the error
+                            val ex = result.getException()
+                            Log.e("GetAllLoans", "API Error: ${ex.message}")
+                            outputTextView.text = "Error: Could not fetch loans from the server"
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // homework
+    private fun getLoansByMemberID(memberId:String){
+       val url = "https://opsc.azurewebsites.net/loans/member/$memberId"
+        outputTextView.text = "Fetching loans for member with ID : $memberId..."
+
+    }
+
+
+    private fun createNewLoan() {
+        val url = "https://opsc.azurewebsites.net/loans/"
+        outputTextView.text = "creating a new loan..."
+        executor.execute {
+            //create a new loan object to send as the request body.
+            val newLoan = LoanPost("15.99", "M6001", "Added by the android app")
+            val jsonBody = Gson().toJson(newLoan)
+
+            url.httpPost()
+                .jsonBody(jsonBody) // set the request body
+                .responseString { _, response, result ->
+                    handler.post {
+                        when (result) {
+                            // a 201 created status code indicates success
+                            is Result.Success -> {
+                                if (response.statusCode == 201) {
+                                    try {
+                                        val createdLoan = Gson().fromJson(
+                                            result.get(),
+                                            Loan::class.java
+                                        )
+
+                                        outputTextView.text =
+                                            "Successfully Created loan: \n\nLoan ID:" +
+                                                    "${createdLoan.LoanID}\n AMount: ${createdLoan.amount}"
+
+                                    } catch (e: JsonSyntaxException) {
+                                        Log.e("CreateNewLoan", "JSON parsing error: ${e.message}")
+                                        outputTextView.text = "could not parse server response."
+                                    }
+                                    } else {
+                                        outputTextView.text =
+                                            "Error: Could not create loan. status: ${response.statusCode}"
+                                    }
+
+                                }
+
+                                is Result.Failure -> {
+                                    val ex = result.getException()
+                                    Log.e("CreateNewLoan", "API Error: ${ex.message}")
+                                    outputTextView.text =
+                                        "Error: Could not create loan. status: ${response.statusCode}"
+                                }
+                            }
+
+                        }
+                    }
+                }
+
+        }
+
+
+
     private fun hideKeyboard(){
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
 
     }
+
 }
